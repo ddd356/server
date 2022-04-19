b0802f08ee9c0980cbe36907c91870a6e1af9bc4
dshk <notabenaabakan@gmail.com>
2022-04-20 00:28:09 +0300

    work

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,52 +1,33 @@
-import Control.Exception ( try )
-import qualified Test.QuickCheck as Q
-import qualified Test.QuickCheck.Monadic as QM
-import Network.HTTP.Simple
-import qualified App as A (app)
-import qualified SQL as S (createNewsDb)
+import qualified Control.Concurrent.Async as Async (withAsync, cancel)
+import qualified Test.QuickCheck as Q (quickCheck, quickCheckWith, Args(..), stdArgs, Property(..))
+import qualified Test.QuickCheck.Monadic as QM (monadicIO, run, assert)
 import qualified Network.Wai.Handler.Warp as W (run)
-import Control.Concurrent.Async ( withAsync, cancel )
-import Database.PostgreSQL.Simple (Binary (..), ConnectInfo (..), close, connect, execute, execute_, query, query_)
-import SQL
-import Data.String (fromString)
-import System.FilePath ((</>))
+import qualified App as A (app)
+import qualified Control.Exception.Base as EB (catch)
+import qualified Control.Exception as E (SomeException(..))
 
-test_database :: String
-test_database = "news_test_database"
+prop_CreateTestDatabase :: Q.Property
+prop_CreateTestDatabase = QM.monadicIO $ do
+        result <- QM.run $ EB.catch ( return True ) ( \e -> return False  )
+        QM.assert result
+--(e :: E.SomeException)
 
-prop_CreateDatabase = QM.monadicIO $ do
-        res <- QM.run           -- m a -> PropertyM m a
-            (try                -- IO (Either e a)
-            (createTestNewsDb)) -- IO ()
-        QM.assert $ case res of
-            Left _ -> False
-            _ -> True
+prop_RemoveTestDatabase :: Bool
+prop_RemoveTestDatabase = True
 
 main :: IO ()
 main = do
-    withAsync (W.run 8080 A.app) $ \a1 -> do
-        --request <- parseRequest "http://localhost:8080/users?action=list"
-        --response <- httpBS request
-        --print response
-        Q.quickCheck prop_CreateDatabase
-        cancel a1
-        return ()
-
-createTestNewsDb :: IO ()
-createTestNewsDb = do
-
-  (host, user, password, _) <- confGetPostgresqlConfiguration
-  conn1 <- connect (ConnectInfo host 5432 user password "")
-  _ <- execute_ conn1 $ fromString $ "CREATE DATABASE " ++ test_database ++ ";"
-  close conn1
-
-  query_text <- readFile $ "src" </> "SQL" </> "migrations" </> "init" </> "init.sql"
-  conn2 <- connect (ConnectInfo host 5432 user password test_database)
-
-  _ <- execute_ conn2 (fromString query_text)
-  close conn2
-
-  updateNewsDb
-  addAdmin
-
-  return ()
+    Async.withAsync (W.run 8080 A.app) $ \a1 ->
+        do
+            Q.quickCheckWith a prop_CreateTestDatabase 
+            Q.quickCheckWith a prop_RemoveTestDatabase
+            Async.cancel a1
+            return () where
+                a = Q.Args {
+                    Q.replay = Q.replay Q.stdArgs,
+                    Q.maxSuccess = 1,
+                    Q.maxDiscardRatio = Q.maxDiscardRatio Q.stdArgs,
+                    Q.maxSize = Q.maxSize Q.stdArgs,
+                    Q.chatty = Q.chatty Q.stdArgs,
+                    Q.maxShrinks = Q.maxShrinks Q.stdArgs
+                    }
